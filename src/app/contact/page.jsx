f314d179fe28b22cbb1a05c004fc3ea7f9a519cc
omar faruk <farukomarcpa@gmail.com>
2024-09-19 18:54:31 +0600

    Review Section added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,7 +7,7 @@ const contactpage = () => {
     return (
         <div className='bg-[#F9F9F9]'>
             <nav className='bg-blue-600 p-10'></nav>
-            <div className='min-h-[90vh] container mx-auto px-4 mt-12'>
+            <div className='container mx-auto px-4 mt-12'>
                 <div className=''>
                 <div>
                     <h1 className='text-4xl font-extrabold opacity-90'>Inform us of Yourself</h1>
@@ -43,6 +43,59 @@ const contactpage = () => {
                         </div>
                     </div>
                 </div>
+                </div>
+                {/* contact from  */}
+                <div>
+                {/* form */}
+                <div>
+                <div className="container mx-auto p-4 bg-white rounded-xl">
+      {/* section header */}
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold mt-6 font-play">
+          Lets Stay In Touch
+        </h1>
+        <p className="leading-7 opacity-80 mt-6 lg:w-2/3 mx-auto">
+          Your brige to meaningful commuication and personalized assistance, we
+          are here to listen and assist you.
+        </p>
+      </div>
+      <div className="p-4 rounded-xl mt-10">
+        <div className="md:flex gap-4">
+          <input
+            type="text"
+            className=" p-4 bg-[#f7f5f4] rounded-lg mt-2  w-full focus:outline-none"
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            className=" p-4 bg-[#f7f5f4] rounded-lg mt-2  w-full focus:outline-none"
+            placeholder="Last Name"
+          />
+        </div>
+        <div className="lg:flex gap-4">
+          <input
+            type="text"
+            className=" p-4 bg-[#f7f5f4] rounded-lg mt-2 w-full focus:outline-none"
+            placeholder="Your Email"
+          />
+        </div>
+        <textarea
+          className=" w-full bg-[#f7f5f4] rounded-lg mt-3 h-52 p-2 focus:outline-none "
+          placeholder="Your messages"
+        ></textarea>
+        <button className="btn w-full bg-[#EA6A12] hover:bg-[#C75A0F] text-white">
+          {" "}
+          Submit
+        </button>
+      </div>
+    </div>
+
+                </div>
+                {/* map  */}
+                <div>
+                    map
+                </div>
+
                 </div>
             </div>
             <footer className='bg-orange-700 p-20'></footer>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Hero from "./component/Hero";
+<<<<<<< HEAD
 import Review from "./Review/Review";
+=======
+import Navbar from "./component/Navbar";
+>>>>>>> 1a8ab25e8dad24f22d380dfc5aa84d83355412ca
 
-export default function Home() {
+export default function aboutus() {
   return (
-    <div className="">
+    <div className="pt-1">
     <Hero/>
     <Review/>
     </div>
