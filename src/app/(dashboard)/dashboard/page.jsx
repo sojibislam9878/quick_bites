9c0c44ee02bf720be2879312948653998b63a0fa
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-09-26 00:56:46 -0700

    update

---
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const dashboardPage = () => {
+    return (
+        <div>
+            <h1>This is dashboardPageest</h1>
+            <h1>This is dashboardPasdafdsgeest</h1>
+        </div>
+    );
+};
+
+export default dashboardPage;
