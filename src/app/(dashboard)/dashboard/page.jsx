7b0df72194e33d9dea3a6f9b319f7a3f856e81c4
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-09-26 01:07:58 -0700

    test

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const dashboardPage = () => {
     return (
         <div>
-            <h1>This is dasadfsfshboardPage</h1>
+            <h1>This is dashboard</h1>
         </div>
     );
 };
