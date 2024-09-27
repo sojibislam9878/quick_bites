7ad5b734b662a1e0079a9ab7ff568d62d998d307
rakib3719 <bannah76769@gmail.com>
2024-09-27 11:19:31 +0600

    all restuarant page some improvement

--- src/app/component/allrestrurent/SingleRestruent.jsx
+++ src/app/component/allrestrurent/SingleRestruent.jsx
@@ -1,62 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const SingleRestruent = ({data}) => {
-
+const SingleRestruent = ({ data }) => {
   return (
-    <div className='xl:w-[50%] lg:w-[80%]  mx-auto '>
-   <div className='bg-white py-5 px-8 sm:flex justify-between items-center'>
+    <div className="xl:w-[50%] lg:w-[80%] mx-auto transition-transform transform hover:scale-105 duration-300">
+      <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 p-6 sm:flex justify-between items-center space-y-4 sm:space-y-0">
+        
+        <div className="flex space-x-4">
+          {/* Image */}
+          <div>
+            <img
+              src={data?.image}
+              alt={data?.name}
+              className="w-[130px] h-[130px] object-cover rounded-md border-2 border-gray-300"
+            />
+          </div>
 
-   <div className='flex '>
-    <div >
+          {/* Restaurant Details */}
+          <div className="pl-2 mt-2">
+            {/* Rating */}
+            <div className="rating mb-2 flex items-center">
+              <input type="radio" name="rating-2" className="mask mask-star-2 bg-yellow-400" />
+              <input type="radio" name="rating-2" className="mask mask-star-2 bg-yellow-400" defaultChecked />
+              <input type="radio" name="rating-2" className="mask mask-star-2 bg-yellow-400" />
+              <input type="radio" name="rating-2" className="mask mask-star-2 bg-yellow-400" />
+              <input type="radio" name="rating-2" className="mask mask-star-2 bg-yellow-400" />
+            </div>
 
-      <img src={data?.image} alt={data?.name} className='w-[130px] p-2 border-2'/>
-    </div>
+            <h1 className="text-xl font-semibold text-gray-800">{data?.name}</h1>
+            <p className="italic text-gray-500">{data?.location}</p>
 
+            <div className="flex text-sm text-gray-600 space-x-2 mt-1">
+              <p>{data?.locationDetail}.</p>
+              <p className="text-red-500 font-medium">Opens at {data?.opensAt}</p>
+            </div>
 
-<div className='pl-6 mt-2'>
+            {/* Mobile Button */}
+            <button className="btn w-full mt-4 sm:hidden bg-rose-500 text-white font-medium rounded-md py-2 shadow hover:bg-rose-600 transition duration-300">
+              View Menu
+            </button>
+          </div>
+        </div>
 
-<div className="rating">
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input
-    type="radio"
-    name="rating-2"
-    className="mask mask-star-2 bg-orange-400"
-    defaultChecked />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-</div>
-
-<h1 className='text-xl  '>{data?.name}</h1>
-<p className='italic text-gray-500'>{data?.location}</p>
-
-<div className='flex text-sm'>
-
-  <p className='text-gray-500'>{data?.locationDetail}.</p>
-  <p className='text-red-600'>  Opens at { data?.opensAt}</p>
-
-</div>
-<div className=''>
-<button className='btn  w-full mt-4 sm:hidden bg-rose-500 text-white'>View Menu</button>
-
-   </div>
-</div>
-
-   </div>
-   <div className='hidden sm:block'>
-<button className='btn   bg-rose-500 text-white'>View Menu</button>
-
-   </div>
-   </div>
-
+        {/* Desktop Button */}
+        <div className="hidden sm:block">
+          <button className="btn bg-rose-500 text-white font-medium rounded-md py-2 px-4 shadow hover:bg-rose-600 transition duration-300">
+            View Menu
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 SingleRestruent.propTypes = {
-  data:PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
 };
 
 export default SingleRestruent;
