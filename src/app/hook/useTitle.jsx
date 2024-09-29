a7c8d06d4f113fc36ba2ae01a47391891119ec44
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-09-29 04:20:41 -0700

    decorate code in dash

---
+++ src/app/hook/useTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const UseTitle = ({heading,description}) => {
+  return (
+    <div className="text-center pt-8">
+    <h1>{heading}</h1>
+    <h3>{description}</h3>
+    
+    </div>
+  )
+}
