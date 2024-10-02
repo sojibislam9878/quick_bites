f22c47f5d7c253d4ea6c364b92604971bf6b15c6
anamikagain559 <anamika.zaimahtech@gmail.com>
2024-10-03 00:20:07 +0600

    add to cart page error solve

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -182,7 +182,7 @@
 "use client";
 
 // import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+
 import { FiAlignJustify } from "react-icons/fi";
 import { GrClose } from "react-icons/gr";
 import { FaUserLarge } from "react-icons/fa6";
@@ -193,8 +193,9 @@ import { FaUserCircle } from "react-icons/fa";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
+import React, { useEffect, useState,useContext  } from "react";
 // import { useRouter } from "next/router";
-
+import CartContext from "../Context/CartContext"
 const navLinks = [
   { path: "/", name: "Home" },
   { path: "/menu", name: "Menu" },
@@ -207,6 +208,8 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+    const { cart } = useContext(CartContext);
+    const cartItems = cart?.cartItems;
   const session = useSession();
   console.log(session);
   const pathName = usePathname();
@@ -305,8 +308,11 @@ const Navbar = () => {
                 <VscSignIn size={25} /> Sign Out
               </div>{" "}
               <div className="w-fit hover:text-orange-600 hover:scale-[1.08] ">
-                <BsCart4 size={25} />
-              </div>{" "}
+           <div className="flex justify-between">   <BsCart4 size={25} />
+                     <span className=" lg:inline ml-1 text-orange-600">
+              (<b>{cartItems?.length || 0}</b>)
+           </span></div>
+           </div>
               <div className="dropdown dropdown-hover">
                 <div tabIndex={0} role="button" className=" m-1">
                   <FaUserCircle
