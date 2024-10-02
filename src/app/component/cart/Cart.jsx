20288027fc1a2da90ce8336f72eef946e4ef3b7d
anamikagain559 <anamika.zaimahtech@gmail.com>
2024-10-03 00:45:16 +0600

    addtocart

--- src/app/component/cart/Cart.jsx
+++ src/app/component/cart/Cart.jsx
@@ -37,6 +37,7 @@ const Cart = () => {
 
   return (
     <>
+    
       <section className="py-5 sm:py-7 bg-blue-100">
         <div className="container max-w-screen-xl mx-auto px-4">
           <h2 className="text-3xl font-semibold mb-2">
