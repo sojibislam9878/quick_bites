f6d5dcde759bf270de2c24444433a284f6c1ba8a
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-10-04 11:38:46 -0700

    updated links

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         <div className="drawer lg:drawer-open">
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex flex-col items-center justify-center">
+          {/* children dashboard */}
             {children}
             <label
               htmlFor="my-drawer-2"
