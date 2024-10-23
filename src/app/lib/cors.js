ac02440ed95b47313d26350a86049482285fad13
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-10-23 19:27:57 +0600

    cors middleware added

---
+++ src/app/lib/cors.js
@@ -0,0 +1,22 @@
+import Cors from 'cors';
+
+
+const cors = Cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  origin: '*', 
+});
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function corsMiddleware(req, res, next) {
+  await runMiddleware(req, res, cors);
+  next();
+}
