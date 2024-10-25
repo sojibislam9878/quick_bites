da73b7018f4bf3e5b226805d62cbebafd8dc9232
rakib3719 <bannah76769@gmail.com>
2024-10-25 12:22:28 +0600

    a simple problem solve

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -25,8 +25,8 @@ const DashboardPage = () => {
   return (
     <div>
       {/* Render components based on the role */}
-      {role === 'usr' && <UserProfile />}
-      {role === 'user' && <AdminProfile />}
+      {role === 'user' && <UserProfile />}
+      {role === 'admin' && <AdminProfile />}
       {role === 'deleveryMan' && <DeleveryProfile />}
       {role === 'owner' && <OwnerProfile />}
     </div>
