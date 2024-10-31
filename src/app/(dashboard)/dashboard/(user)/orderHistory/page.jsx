ed281e0023a34969bbc6436db05738f118b1d2ee
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-10-31 13:43:19 -0700

    updateing

--- src/app/(dashboard)/dashboard/(user)/orderHistory/page.jsx
+++ src/app/(dashboard)/dashboard/(user)/orderHistory/page.jsx
@@ -34,7 +34,7 @@ const OrderHistory = () => {
       <div className="max-w-screen-xl mx-auto px-4 md:px-8 bg-white rounded-lg shadow-md">
    <div className="max-w-2xl mx-auto text-center">
 <h3 className="text-gray-600 text-3xl font-semibold sm:text-4xl">
-    All Order History
+    All Order  History
 </h3>
 
 </div>
